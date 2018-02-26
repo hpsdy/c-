2afdf479e265dea643d8f5695eec9b7808fb20e0
qinhan <qinhan@baidu.com>
2018-02-26 15:01:47 +0800

    add

--- 13/lval.cpp
+++ 13/lval.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 #include<utility>
 using namespace std;
-int main(){
-	const int  &a = 100;
-	cout<<a<<endl;
-	int &&c = 20;
-	int &&b = std::move(c);
-	cout<<b<<endl;
-	cout<<c<<endl;
-	b = 99;
-	cout<<"=-="<<endl;
-	cout<<&b<<endl;
-	cout<<&c<<endl;
-
-	return 0;
+int main() {
+    const int  &a = 100;
+    cout << a << endl;
+    int &&c = 20;
+    int &&b = std::move(c);
+    cout << b << endl;
+    cout << c << endl;
+    b = 99;
+    cout << "=-=" << endl;
+    cout << &b << endl;
+    cout << &c << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
 }
