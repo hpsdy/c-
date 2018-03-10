be2e7342ca84c7e4d0bdd59cc318deaf121d4e1a
qinhan <1036474541@qq.com>
2018-03-10 12:56:44 +0800

    add

--- 13/strvec/strT.cpp
+++ 13/strvec/strT.cpp
@@ -5,7 +5,12 @@
 int main() {
     std::vector<user::String> arr;
     arr.push_back("abc");
-    arr.push_back("qinhan");
+    std::cout << "================" << std::endl;
+    user::String str = "qinhan";
+    arr.push_back(str);
+    std::cout << "================" << std::endl;
+    user::String str1 = "dashuaige";
+    arr.push_back(std::move(str));
     for (auto b = arr.begin(), e = arr.end(); b != e; ++b) {
         std::cout << *b << std::endl;
     }
