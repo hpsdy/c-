b5e9bec5fc64764797577a8ec41c140c827981da
qinhan <1036474541@qq.com>
2018-03-10 14:58:18 +0800

    add

--- 13/strvec/strT.cpp
+++ 13/strvec/strT.cpp
@@ -1,4 +1,5 @@
 #include "string.h"
+#include "strVec.h"
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -19,6 +20,24 @@ int main() {
     for (auto b = arr.begin(), e = arr.end(); b != e; ++b) {
         std::cout << *b << std::endl;
     }
-
+    std::cout << "================" << std::endl;
+    std::cout << "================" << std::endl;
+    std::cout << "================" << std::endl;
+    strVec<user::String> strarr;
+    strarr.push_back("abc");
+    std::cout << "================" << std::endl;
+    user::String str = "qinhan";
+    std::cout << "================" << std::endl;
+    strarr.push_back(str);
+    std::cout << "================" << std::endl;
+    user::String str1 = "dashuaige";
+    std::cout << "================" << std::endl;
+    user::String &&p = std::move(str);
+    std::cout << "================" << std::endl;
+    strarr.push_back(std::move(str));
+    std::cout << "================" << std::endl;
+    for (auto b = strarr.begin(), e = strarr.end(); b != e; ++b) {
+        std::cout << *b << std::endl;
+    }
     return EXIT_SUCCESS;
 }
