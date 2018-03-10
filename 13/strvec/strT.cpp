aef488e22d8be24f4c728bfa6a56da478c01784e
qinhan <1036474541@qq.com>
2018-03-10 12:48:15 +0800

    add

--- 13/strvec/strT.cpp
+++ 13/strvec/strT.cpp
@@ -1,8 +1,9 @@
 #include "string.h"
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 int main() {
-    std::vector<String> arr;
+    std::vector<user::String> arr;
     arr.push_back("abc");
     arr.push_back("qinhan");
     for (auto b = arr.begin(), e = arr.end(); b != e; ++b) {
