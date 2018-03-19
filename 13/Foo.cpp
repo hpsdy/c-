4a0f6a27e14defaf98e9367a57ad9b2971bad860
qinhan <qinhan@baidu.com>
2018-03-19 19:58:36 +0800

    add

--- 13/Foo.cpp
+++ 13/Foo.cpp
@@ -5,24 +5,24 @@ using namespace std;
 class Foo {
   public:
     Foo() {
-        cout << "默认构造函数" << endl;
+        cout << "默认构造函数:" << this << endl;
     };
     Foo(const Foo &f) {
-        cout << "拷贝构造函数" << endl;
+        cout << "拷贝构造函数:" << this << endl;
         data = f.data;
     }
     Foo(Foo &&f) {
-        cout << "移动构造函数" << endl;
+        cout << "移动构造函数:" << this << endl;
         data = std::move(f.data);
     }
     Foo sorted() &&;
     Foo sorted() const &;
     ~Foo() {
-        cout << "析构函数" << endl;
+        cout << "析构函数:" << this << endl;
         data.clear();
     }
     void print() {
-        cout << "print" << endl;
+        cout << "print:" << this << endl;
         for (auto &c : data) {
             cout << c << endl;
         }
@@ -32,12 +32,12 @@ class Foo {
 
 };
 Foo Foo::sorted() && {
-    cout << "右值引用版本" << endl;
+    cout << "右值引用版本:" << this << endl;
     sort(data.begin(), data.end());
     return *this;
 }
 Foo Foo::sorted() const & {
-    cout << "左值引用版本" << endl;
+    cout << "左值引用版本:" << this << endl;
     //Foo ret(*this);
     //return ret.sorted();
     return Foo(*this).sorted();
