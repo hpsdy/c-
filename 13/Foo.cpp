9d7d6cace67a64a75dabb62b59e5b13b92bc6502
qinhan <qinhan@baidu.com>
2018-03-19 19:26:24 +0800

    add

--- 13/Foo.cpp
+++ 13/Foo.cpp
@@ -4,7 +4,17 @@
 using namespace std;
 class Foo {
   public:
-    Foo() = default;
+    Foo() {
+        cout << "默认构造函数" << endl;
+    };
+    Foo(const Foo &f) {
+        cout << "拷贝构造函数" << endl;
+        data = f.data;
+    }
+    Foo(Foo &&f) {
+        cout << "移动构造函数" << endl;
+        data = std::move(f.data);
+    }
     Foo sorted() &&;
     Foo sorted() const &;
     ~Foo() = default;
