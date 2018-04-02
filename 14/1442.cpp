e3b08d8aafcb46b0c74995106049c2a802c58b78
qinhan <qinhan@baidu.com>
2018-04-02 23:15:12 +0800

    ad

--- 14/1442.cpp
+++ 14/1442.cpp
@@ -16,20 +16,14 @@ int main(int argc, char const *argv[]) {
     std::cout << num << std::endl;
     auto fir = std::find_if(vec.cbegin(), vec.cend(), std::bind(std::not_equal_to<std::string>(), std::placeholders::_1, "han"));
     std::cout << *fir << std::endl;
-    std::for_each(vec.begin(), vec.end(), std::bind(std::plus<std::string>(), "pref", std::placeholders::_2));
+    std::for_each(vec.begin(), vec.end(), std::bind(std::plus<std::string>(), "pref", std::placeholders::_1));
     std::cout << "========" << std::endl;
     for (auto &c : vec) {
         std::cout << c << std::endl;
     }
     std::vector<int> intarr{2, 4, 6, 8, 10};
-    num = std::count_if(vec.cbegin(), vec.cend(), std::bind(std::modulus<int>(), 10, std::placeholders::_2));
+    num = std::count_if(intarr.cbegin(), intarr.cend(), std::bind(std::modulus<int>(), 10, std::placeholders::_1));
     std::cout << num << std::endl;
-    input(std::istream & io, std::vector<std::string> &vec) *p = std::bind(input);
-    std::vector<std::string> vec1;
-    p(std::cin, vec1);
-    std::cout << "========" << std::endl;
-    for (auto &c : vec1) {
-        std::cout << c << std::endl;
-    }
+ 
     return 0;
 }
