8f6501054762d526c128cb139e0da3ed84415209
qinhan <qinhan@baidu.com>
2018-04-02 21:53:38 +0800

    ad

--- 14/1442.cpp
+++ 14/1442.cpp
@@ -12,10 +12,12 @@ void input(std::istream &io, std::vector<std::string> &vec) {
 int main(int argc, char const *argv[]) {
     std::vector<std::string> vec;
     input(std::cin, vec);
-    std::count_if(vec.cbegin(), vec.cend(), bind(std::equal_to<std::string>(), std::placeholders::_1, "qin"));
+    int num = std::count_if(vec.cbegin(), vec.cend(), bind(std::equal_to<std::string>(), std::placeholders::_1, "qin"));
+    std::cout << num << std::endl;
     auto fir = std::find_if(vec.cbegin(), vec.cend(), bind(std::not_equal_to<std::string>(), std::placeholders::_1, "han"));
     std::cout << *fir << std::endl;
-    std::for_each(vec.cbegin(), vec.cend(), bind(std::plus<std::string>(), "pref", std::placeholders::_2));
+    std::for_each(vec.begin(), vec.end(), bind(std::plus<std::string>(), "pref", std::placeholders::_2));
+    std::cout << "========" << std::endl;
     for (auto &c : vec) {
         std::cout << c << std::endl;
     }
