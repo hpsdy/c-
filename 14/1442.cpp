31ab1dce4ea40dfd301dc0bc6967be76898d1d25
qinhan <qinhan@baidu.com>
2018-04-02 22:07:17 +0800

    ad

--- 14/1442.cpp
+++ 14/1442.cpp
@@ -12,13 +12,23 @@ void input(std::istream &io, std::vector<std::string> &vec) {
 int main(int argc, char const *argv[]) {
     std::vector<std::string> vec;
     input(std::cin, vec);
-    int num = std::count_if(vec.cbegin(), vec.cend(), bind(std::equal_to<std::string>(), std::placeholders::_1, "qin"));
+    int num = std::count_if(vec.cbegin(), vec.cend(), std::bind(std::equal_to<std::string>(), std::placeholders::_1, "qin"));
     std::cout << num << std::endl;
-    auto fir = std::find_if(vec.cbegin(), vec.cend(), bind(std::not_equal_to<std::string>(), std::placeholders::_1, "han"));
+    auto fir = std::find_if(vec.cbegin(), vec.cend(), std::bind(std::not_equal_to<std::string>(), std::placeholders::_1, "han"));
     std::cout << *fir << std::endl;
-    std::for_each(vec.begin(), vec.end(), bind(std::plus<std::string>(), "pref", std::placeholders::_2));
+    std::for_each(vec.begin(), vec.end(), std::bind(std::plus<std::string>(), "pref", std::placeholders::_2));
     std::cout << "========" << std::endl;
     for (auto &c : vec) {
+        std::cout << c << std::endl;
+    }
+    std::vector<int> intarr{2, 4, 6, 8, 10};
+    num = std::count_if(vec.cbegin(), vec.cend(), std::bind(std::modulus<int>(), 10, std::placeholders::_2));
+    std::cout << num << std::endl;
+    input(std::istream & io, std::vector<std::string> &vec) *p = std::bind(input);
+    std::vector<std::string> vec1;
+    p(std::cin, vec1);
+    std::cout << "========" << std::endl;
+    for (auto &c : vec1) {
         std::cout << c << std::endl;
     }
     return 0;
