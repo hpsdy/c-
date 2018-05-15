4b4edebd4d568ba6b9b141463ca0389bf6ea6895
qinhan <qinhan@baidu.com>
2018-05-15 22:57:13 +0800

    add

---
+++ 15/text/include/OrQuery.h
@@ -0,0 +1,8 @@
+//
+// Created by qinhan on 2018/5/14.
+//
+
+#ifndef INC_15_ORQUERY_H
+#define INC_15_ORQUERY_H
+
+#endif //INC_15_ORQUERY_H
---
+++ 15/text/include/query_base.h
@@ -0,0 +1,12 @@
+//
+// Created by qinhan on 2018/5/14.
+//
+
+#ifndef INC_15_QUERY_BASE_H
+#define INC_15_QUERY_BASE_H
+class query_base{
+    friend class Query;
+    protected:
+    using line_no =
+};
+#endif //INC_15_QUERY_BASE_H
