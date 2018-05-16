ea74090eda1e6e07d897ee118bbd0b090ca9f22b
qinhan <qinhan@baidu.com>
2018-05-16 21:51:53 +0800

    add

--- 15/class/a.h
+++ 15/class/a.h
@@ -2,10 +2,12 @@
 // Created by qinhan on 2018/5/16.
 //
 
-/*#ifndef C_PLUS_A_H
+#ifndef C_PLUS_A_H
 #define C_PLUS_A_H
 
-#endif //C_PLUS_A_H*/
+
 class a {
     void p();
 };
+
+#endif //C_PLUS_A_H
--- 15/class/b.h
+++ 15/class/b.h
@@ -2,12 +2,15 @@
 // Created by qinhan on 2018/5/16.
 //
 
-/*#ifndef C_PLUS_B_H
+#ifndef C_PLUS_B_H
 #define C_PLUS_B_H
 
-#endif //C_PLUS_B_H*/
 #include "a.h"
 
 class b : public a {
     void p();
 };
+
+#endif //C_PLUS_B_H
+
+
