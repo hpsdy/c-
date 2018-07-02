b7dbd6bff45392803afc1c16fb85643110e823a1
qinhan <qinhan@baidu.com>
2018-07-02 16:50:16 +0800

    add

--- 16/1612.cpp
+++ 16/1612.cpp
@@ -2,16 +2,8 @@
 // Created by qinhan on 2018/7/2.
 //
 #include "1612.h"
-class test{
-    test(int a,int b){
-        printf("%d|%d",a,b);
-    }
-};
 
 int main() {
-    test(10,20);
-    test{9,90};
-    test({10,20});
     try {
         Blob<std::string> arr{"hello", "word"};
         Blob<std::string>::size_type size = arr.size();
@@ -19,7 +11,7 @@ int main() {
         for (i = 0; i < size; ++i) {
             printf("index:%d,value:%s\n", i, arr[i].c_str());
         }
-    } catch (std::except e) {
+    } catch (std::exception e) {
         printf("except:%s\n", e.what());
     } catch (...) {
         printf("except:%s\n", "δ֪�쳣");
